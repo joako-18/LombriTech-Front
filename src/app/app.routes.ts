1b9a169e69e3d4801403540766a6b9189b6fedd6
joako-18 <joako1327@gmail.com>
2025-06-12 09:30:42 -0600

    archivo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { ConfiguracionComponent } from './pages/configuracion/configuracion.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+import { HomeDashboardComponent } from './pages/home-dashboard/home-dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'acerca-de', component: AcercaDeComponent },
+    { path: 'configuracion', component: ConfiguracionComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'estadisticas', component: EstadisticasComponent },
+    { path: 'home-dashboard', component: HomeDashboardComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'registro', component: RegistroComponent }
+];
+
