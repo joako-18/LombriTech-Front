d9440e16de3ce885a1a2c5711fb43ecdb0c20689
joako-18 <joako1327@gmail.com>
2025-06-19 21:40:01 -0600

    estructura de carpetas con archivo de ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
