314bb03663a78239461fdd5809a7364aa4bdbb60
joako-18 <joako1327@gmail.com>
2025-07-02 20:57:31 -0600

    metodo para ir al home del dashboar

--- src/app/features/user/pages/login/login.component.ts
+++ src/app/features/user/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/home-dashboard']);
+  }
 }
