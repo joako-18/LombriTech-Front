f2055ad1a9614c2344d6b1c290591173afc4e28f
joako-18 <joako1327@gmail.com>
2025-07-02 20:59:26 -0600

    componente sidevar

---
+++ src/app/features/dashboard/components/navigate/navigate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigate',
+  imports: [],
+  templateUrl: './navigate.component.html',
+  styleUrl: './navigate.component.css'
+})
+export class NavigateComponent {
+
+}
