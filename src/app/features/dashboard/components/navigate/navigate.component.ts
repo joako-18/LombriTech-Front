e81b8c78ed3ecc0b510bce5ec69f2352e3f3bf28
joako-18 <joako1327@gmail.com>
2025-07-13 18:05:24 -0600

    errores de la sidebar

--- src/app/features/dashboard/components/navigate/navigate.component.ts
+++ src/app/features/dashboard/components/navigate/navigate.component.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
 })
 export class NavigateComponent {
   sidebarOpen = false;
-  mostrarEstadisticas = false;
+  mostrarEstadisticas = true;
 
   constructor(public router: Router) {}
 
